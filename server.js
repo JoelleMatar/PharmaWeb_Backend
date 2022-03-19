5791eeaba1d1b11bba87f9c3a98aa19ec88740ee
JoelleMatar <71333209+JoelleMatar@users.noreply.github.com>
2022-03-19 11:29:37 +0200

    Creating Backend Environment

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} Database Connection Failed`));
